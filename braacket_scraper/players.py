6796addd37401c8c7760501e1238cc52c12de015
Benji Spetter-Goldstein <benjisg@comcast.net>
2022-04-03 14:02:49 -0700

    Add Louis

--- braacket_scraper/players.py
+++ braacket_scraper/players.py
@@ -14,3 +14,4 @@ class Player:
 # create all players
 DrLobster = Player('6B2BEB69-DCE1-4095-BFED-4F3205CF3CDB', 'DrLobster')
 Kalvar = Player('C2069576-DB5B-42E0-9C4B-31182802A60F', 'Kalvar')
+Louis = Player('4C2B18BA-75A9-45ED-A8B6-8D891297E00A'. 'Louis')
