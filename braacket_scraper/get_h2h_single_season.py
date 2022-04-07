a826341fcc72d03e02a36c8b0bc40c7446807e74
Benji Spetter-Goldstein <benjisg@comcast.net>
2022-04-07 14:13:16 -0700

    Read out set history

--- braacket_scraper/get_h2h_single_season.py
+++ braacket_scraper/get_h2h_single_season.py
@@ -25,3 +25,26 @@ game_wins = job_elements[3].text
 game_losses = job_elements[4].text
 
 print(player1.name + " " + set_wins + " - " + set_losses + " " + player2.name)
+print()
+print()
+
+URL = "https://braacket.com/league/" + player1.league.id + "/player/" + player1.id + "?ranking=" + ranking.id
+page = requests.get(URL)
+
+soup = BeautifulSoup(page.content, "html.parser")
+results = soup.find(id="content_body")
+
+job_elements = results.find_all("tr", class_="my-table-row-action")
+
+for job_element in job_elements:
+    
+    sibling = job_element
+    sibling = sibling.next_sibling
+    while((len(sibling) == 1 or len(sibling) == 9)):
+        if player2.name in str(sibling):
+            print(" ".join(job_element.text.strip().split()))
+            print(" ".join(sibling.text.strip().split()))
+            print()
+        sibling = sibling.next_sibling
+        if(sibling == None):
+            break
